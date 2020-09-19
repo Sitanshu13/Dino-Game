6f5e9acbd3bf6cfe6abec3a96fc7a74d24451580
Sitanshu <69389824+Sitanshu13@users.noreply.github.com>
2020-09-19 16:10:35 +0530

    Update script.js

--- Dino Game/script.js
+++ Dino Game/script.js
@@ -13,7 +13,7 @@ function jump() {
 var checkDead = setInterval(function (){
     var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
     var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if (blockLeft<20 && blockLeft>0 && characterTop>=130){
+    if (blockLeft<52 && blockLeft>0 && characterTop>=130){
         block.style.animation = "none";
         block.style.display = "none";
         alert("U lose")
